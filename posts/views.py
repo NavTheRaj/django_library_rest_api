9455025fd252503a4ec5e334b5c9beac81be7161
NavTheRaj <navrajkhanal61@gmail.com>
2020-05-07 13:51:58 +0545

    Added api with permission functions

---
+++ posts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer
+from rest_framework import generics
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer 
+
